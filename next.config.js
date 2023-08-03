024408322fd5e55b4188827cfddda5f83fa1a092
heiwa4126 <heiwa4126@gmail.com>
2023-08-03 14:02:01 +0900

    fix github pages bug

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
-// const output = process.env.NEXTCONFIG_OUTPUT
 
 // GuHub Pages specail
-const githubBasePath = process.env.GITHUB_REPOSITORY?.split('/')[1];
+function s(s) {
+  s = s?.split('/')[1];
+  return s ? '/' + s : undefined;
+}
 
-const basePath = githubBasePath ?? process.env.NEXTCONFIG_BASEPATH ?? '';
+const basePath = s(process.env.GITHUB_REPOSITORY) ?? process.env.NEXTCONFIG_BASEPATH ?? '';
 exports.basePath = basePath;
 
 console.log(`*** basePath=${basePath}`);
 
 const nextConfig = {
-  output: 'export',
+  output: 'export', // process.env.NEXTCONFIG_OUTPUT
   basePath
 };
 
